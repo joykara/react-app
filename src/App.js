9cee225709b87e8e80b749a6eecd0b6267f79d0c
joykara <joykarani1@gmail.com>
2023-05-27 10:37:56 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Header, Navbar } from './components';
 
 function App() {
   return (
@@ -15,9 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is React
         </a>
       </header>
+      <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+//export branches from components
+export { default as Header } from './header/Header';
+export { default as Navbar } from './navbar/Navbar';

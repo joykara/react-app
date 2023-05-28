85a79abc9ab8cd3fd1bd45da9e0b4838c2870878
joykara <joykarani1@gmail.com>
2023-05-28 21:37:39 +0300

    ...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {RiArrowRightLine} from 'react-icons/ri'
+import './header.css'
 
 const Header = () => {
-  return (
-    <div>Header</div>
+return (
+    <div className='header section__padding' id='home'>
+
+      <div className="header-content">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+            <div className="header-content__button">
+                {/* TODO: Make this a p tag to test transformation */}
+                <button type='button'>Learn More <span className='arrow'><RiArrowRightLine size={27}/></span></button>
+        </div>
+      </div>
+
+      <div className="header-image">
+
+      </div>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { RiMenuLine, RiCloseLine, RiSearchLine, RiArrowRightSLine } from 'react-icons/ri';
+import './navbar.css'
 
+//create McKinsey & Company menu
+//create About menu
+//create Careers menu
+const Menu = () => (
+    <>
+        <p><a href="#industries">Industries</a></p>
+        <p><a href="#capabilities">Capabilities</a></p>
+        <p><a href="#features">Featured Insights</a></p>
+        <p><a href="#locations">Locations</a></p>
+        <p><a href="#career">Careers</a></p>
+        <p><a href="#about">About Us</a></p>
+        <p><a href="#blog">Blog</a></p>
+    </>
+)
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    const handleToggle = () => {
+        setToggleMenu(!toggleMenu);
+      };
+
+    return (
+        <div className="navbar">
+            <div className="navbar__logo">
+                <button onClick={handleToggle}>
+                    {toggleMenu ? <RiCloseLine color="#fff" size={30} /> : <RiMenuLine color="#fff" size={30} />}
+                </button>
+                <p>McKinsey<br/> &nbsp; & Company</p>
+            </div>
+            {toggleMenu && (
+                <div className={`navbar__menu-links ${toggleMenu ? 'show' : ''}`}>
+                    <div className="navbar__menu-links__container">
+                        <div className="navbar__menu-links-top">
+                            <button onClick={handleToggle}>
+                                {toggleMenu ? <RiCloseLine color="#fff" size={45} /> : <RiMenuLine color="#fff" size={30} />}
+                            </button>
+                            <p>McKinsey<br/> &nbsp; & Company</p>
+                        </div>
+                        <div className="menu">
+                            <Menu />
+                            <p><a href="#subscribe">Email Subscriptions</a></p>
+                            <p><a href="#sign-in">Sign In</a></p>
+                        </div>
+                    </div>
+                    <div className="navbar__menu-links__details">
+                        <div className="navbar__menu-links__search">
+                            <input type="text" />
+                            <button><RiSearchLine color='#fff' size={40}/></button>
+                        </div>
+                    </div>
+                </div>
+            )}
+            <div className="navbar__menu">
+                <Menu />
+            </div>
+            <div className="navbar__search">
+                <p><a href="#industries">Sign In</a> | <a href="#industries">Subscribe</a></p>
+                <p><a href="#industries"><RiSearchLine size={40}/></a></p>
+            </div>
+        </div>
   )
 }
 
